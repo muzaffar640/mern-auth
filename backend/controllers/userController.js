069c4065d55e378b20ecd96f2f59d6aa6f7f5800
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-24 11:30:29 +0530

    Added express async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,10 @@
+import asyncHandler from "express-async-handler";
 // desc Auth user/set token
 // routes POST /api/users/auth
 // access Public
 
-const authUser = async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
 export { authUser };
