2949c9febfa67196505e70d94d8d2edad03bf7dc
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-25 14:48:08 +0530

    Add login user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,6 +7,28 @@ import generateToken from "../utils/generateToken.js";
 // access Public
 
 const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email  or password");
+  }
+});
+
+// desc Register a new user
+// routes POST /api/users
+// access Public
+
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   const userExists = await User.findOne({ email });
@@ -33,14 +55,6 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid user data");
   }
-});
-
-// desc Register a new user
-// routes POST /api/users
-// access Public
-
-const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
 });
 
 // desc Logout user / clear cookie
