3ba421d4bb98c04aa05e204b9f5a14ffd531e611
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-25 12:38:11 +0530

    Add register user endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,36 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // desc Auth user/set token
 // routes POST /api/users/auth
 // access Public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // desc Register a new user
