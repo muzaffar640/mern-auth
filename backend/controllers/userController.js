a6d05f91be8af37e114312f125aa703fbe7ecb2d
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-25 13:12:56 +0530

    Fix a typo, generate jwt and save cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // desc Auth user/set token
 // routes POST /api/users/auth
@@ -22,6 +23,7 @@ const authUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
     },
   },
   {
-    timesatamps: true,
+    timestamps: true,
   }
 );
 
