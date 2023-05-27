aac9f9a090d1a9aadcb90eedc1b76ce321dbd453
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-27 10:44:18 +0530

    Add Home screen and Hero sesction and react router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <>
       <Header />
-      <h4>MERN AUTH</h4>
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 }
