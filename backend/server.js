3ed8ac3ec847cafab54708a16d512a37d387886c
Muzaffar Hossain <muzaffar640@gmail.com>
2023-05-28 11:45:07 +0530

    Add condition for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -16,7 +17,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Server is ready to serve"));
+if (process.env.NODE_ENV === "production") {
+  console.log("production");
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
